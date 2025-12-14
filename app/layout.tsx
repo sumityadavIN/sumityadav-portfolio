3b16bdd3874a04c6c948561fdb42755575cee2c2
Sumit Yadav <yadavsumit18@gmail.com>
2025-12-14 06:53:20 +0530

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,8 @@
+import Link from 'next/link'
+
+<header className="border-b">
+  <nav className="mx-auto max-w-4xl px-6 py-4 flex gap-6">
+    <Link href="/">Home</Link>
+    <Link href="/">Blog</Link>
+  </nav>
+</header>
