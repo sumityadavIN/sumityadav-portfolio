569690971b81b2ff8f4cd95d0798f76fd26f7121
Sumit Yadav <yadavsumit18@gmail.com>
2025-12-14 07:11:37 +0530

    Update page.tsx

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -46,13 +46,13 @@ function HeroPost({
 >) {
   return (
     <article>
-      <Link className="group mb-8 block md:mb-16" href={`/posts/${slug}`}>
+      <Link className="group mb-8 block md:mb-16" href={`/blog/posts/${slug}`}>
         <CoverImage image={coverImage} priority />
       </Link>
       <div className="mb-20 md:mb-28 md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8">
         <div>
           <h3 className="text-pretty mb-4 text-4xl leading-tight lg:text-6xl">
-            <Link href={`/posts/${slug}`} className="hover:underline">
+            <Link href={`/blog/posts/${slug}`} className="hover:underline">
               {title}
             </Link>
           </h3>
