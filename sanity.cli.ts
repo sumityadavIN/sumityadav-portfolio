369713cb757a1ce2b94ebb7de0c23f3441b9af03
Sumit Yadav <yadavsumit18@gmail.com>
2025-12-14 05:58:21 +0530

    Update sanity.cli.ts

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,13 +1,19 @@
+import { defineCliConfig } from "sanity/cli";
 
-import { defineCliConfig } from 'sanity/cli'
-
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID |
-
-| 'peswtk04'
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET |
-
-| 'production'
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 
 export default defineCliConfig({
-  api: { projectId, dataset }
-})
+  api: { projectId, dataset },
+  vite: {
+    define: {
+      "process.env.NEXT_PUBLIC_SANITY_PROJECT_ID": JSON.stringify(projectId),
+      "process.env.NEXT_PUBLIC_SANITY_DATASET": JSON.stringify(dataset),
+    },
+    resolve: {
+      alias: {
+        "@": __dirname,
+      },
+    },
+  },
+});
